dbf6d49de989def48f87a192787907874d3b6bae
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2024-01-20 21:02:57 -0300

    Login de Administrador com o framework Bootstrap

--- admin_login.php
+++ admin_login.php
@@ -11,26 +11,29 @@
 <body>
     <div class="container">
         <form action="">
+            <h2>Login de Administrador</h2>
             <div class="row mb-3">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">E-mail</label>
+                <label for="email" class="col-sm-2 col-form-label">E-mail</label>
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3">
+                    <input type="email" class="form-control" name="email" id="email">
                 </div>
             </div>
             <div class="row mb-3">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
+                <label for="pwd" class="col-sm-2 col-form-label">Senha</label>
                 <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword3">
+                    <input type="password" class="form-control" name="password" id="pwd">
                 </div>
             </div>
-            <div class="col-auto">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="autoSizingCheck">
-                    <label class="form-check-label" for="autoSizingCheck">
-                        Lembra-me
-                    </label>
+            <!--
+                <div class="col-auto">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="autoSizingCheck">
+                        <label class="form-check-label" for="autoSizingCheck">
+                            Lembra-me
+                        </label>
+                    </div>
                 </div>
-            </div>
+            -->
             <div class="col-auto">
                 <button type="submit" class="btn btn-primary">Login</button>
             </div>
